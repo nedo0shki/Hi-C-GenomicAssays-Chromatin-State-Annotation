33cf00f86f7ae2d9579c2a1ddf2d8314ca89958c
nedo0shki <nedashokraneh95@gmail.com>
2020-01-17 16:38:21 -0800

    make_wig script has been added

---
+++ src/utilities/make_wig.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python make_wig.py --data_path "/media/nedooshki/f4f0aea6-900a-437f-82e1-238569330477/SNIPER/SNIPER/encoded_data(128).mat" --index_path "/media/nedooshki/f4f0aea6-900a-437f-82e1-238569330477/SNIPER/SNIPER/crop_map/cropMap.mat" --LV_wig_dir_path "/media/nedooshki/f4f0aea6-900a-437f-82e1-238569330477/Hi-C+ChIP-seq-Chromatin-State-Annotation/data/LV" --resolution 100000
